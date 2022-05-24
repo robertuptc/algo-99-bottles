be8e329bcbaf6699f039f75406578dd1488f94d4
robertuptc <robertuptc@gmail.com>
2022-05-24 09:24:36 -0400

    finish the exercise

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,13 @@
-function bottleSong() {
-  // Write your code here!
+function bottleSong(num) {
+  let song = ""
+for (let i = num; i > 0; i--) {
+  i > 1 ? song +=  `${i} bottles of beer on the wall, ${i} bottles of beer. \n Take one down and pass it around, ${i-1} bottles of beer on the wall. \n `
+  : song +=  `${i} bottle of beer on the wall, ${i} bottle of beer. \n Take one down and pass it around, \n No more bottles of beer on the wall, no more bottles of beer. \n Go to the store and buy some more, 99 bottles of beer on the wall. `
+} return song
 };
 
-bottleSong();
+console.log(bottleSong(10))
+
+
+// 99 bottles of beer on the wall, 99 bottles of beer.
+// Take one down and pass it around, 98 bottles of beer on the wall.
